7082f150f5330462b1cf437a3f26bea2f4382e51
Alexandr Marchenko <marchenko.alexandr@gmail.com>
2026-03-01 10:26:19 +0200

    do not overwrite production appsettings while rsyncing files

--- deploy.sh
+++ deploy.sh
@@ -4,13 +4,13 @@ dotnet publish -c Release -r linux-x64 --self-contained true
 ssh clientportal 'systemctl stop gateway'
 
 # Getaway
-rsync -vz --delete --recursive Gateway/bin/Release/net*/linux-x64/publish/ clientportal:/opt/gateway/
+rsync -vz --delete --recursive --exclude='appsettings.Production.json' Gateway/bin/Release/net*/linux-x64/publish/ clientportal:/opt/gateway/
 
 # CookieGateway
-rsync -vz --delete --recursive CookieGateway/bin/Release/net*/linux-x64/publish/ clientportal:/opt/gateway/
+rsync -vz --delete --recursive --exclude='appsettings.Production.json' CookieGateway/bin/Release/net*/linux-x64/publish/ clientportal:/opt/gateway/
 
 # Feed
-rsync -vz --delete --recursive Feed/bin/Release/net*/linux-x64/publish/ clientportal:/opt/feed/
+rsync -vz --delete --recursive --exclude='appsettings.Production.json' Feed/bin/Release/net*/linux-x64/publish/ clientportal:/opt/feed/
 
 
 ssh clientportal 'systemctl start gateway'
