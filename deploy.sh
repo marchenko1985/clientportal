f8990538be5ea6dad686ec0499a909a593d0f6aa
Alexandr Marchenko <marchenko.alexandr@gmail.com>
2026-03-17 11:29:02 +0200

    new server name

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,16 @@
 # publish
 dotnet publish -c Release -r linux-x64 --self-contained true
 
-ssh clientportal 'systemctl stop gateway'
+ssh optionslab 'systemctl stop gateway'
 
 # Getaway
-rsync -vz --delete --recursive --exclude='appsettings.Production.json' Gateway/bin/Release/net*/linux-x64/publish/ clientportal:/opt/gateway/
+rsync -vz --delete --recursive --exclude='appsettings.Production.json' Gateway/bin/Release/net*/linux-x64/publish/ optionslab:/opt/gateway/
 
 # CookieGateway
-rsync -vz --delete --recursive --exclude='appsettings.Production.json' CookieGateway/bin/Release/net*/linux-x64/publish/ clientportal:/opt/gateway/
+rsync -vz --delete --recursive --exclude='appsettings.Production.json' CookieGateway/bin/Release/net*/linux-x64/publish/ optionslab:/opt/gateway/
 
 # Feed
-rsync -vz --delete --recursive --exclude='appsettings.Production.json' Feed/bin/Release/net*/linux-x64/publish/ clientportal:/opt/feed/
+rsync -vz --delete --recursive --exclude='appsettings.Production.json' Feed/bin/Release/net*/linux-x64/publish/ optionslab:/opt/feed/
 
 
-ssh clientportal 'systemctl start gateway'
+ssh optionslab 'systemctl start gateway'
